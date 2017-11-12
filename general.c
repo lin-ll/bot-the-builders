94941cbfa033bc7694551bf59173bc71cf6c5f26
czye17 <czye@princeton.edu>
2017-11-12 13:39:24 -0500

    starting to work on API for movement and stuff

---
+++ general.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+// moves a given distance c forwards
+void forwards(int c) {
+
+}
+
+// moves left distance c
+void left(int c) {
+
+}
+
+// moves right distance c
+void right(int c) {
+
+}
+
+// moves backwards distance c
+void back(int c) {
+
+}
+
+// reads distance from long
+double checkDistanceFront() {
+
+}
+
+double checkDistanceBack() {
+
+}
+
+double checkDistanceLeft() {
+
+}
+
+double checkDistanceRight() {
+
+}
+
+// returns value read from sensor s
+double sensorLong(int s) {
+    // something to distinguish short and long sensors?
+}
+
+int main(int argc, char *argv[]) {
+}
