bbd6b1db23ebb597d7949ebfa9b7a127a948a8cf
Will Hess <whess@princeton.edu>
2017-11-19 12:47:17 -0500

    Added maze.c

---
+++ maze.c
@@ -0,0 +1,8 @@
+/*----------------------------------------------------------------------------*/
+/* Maze solving and traversal. Calls on general.c methods for sensor reading  */
+/* and motor control.                                                         */
+/*----------------------------------------------------------------------------*/
+
+int main(int argc, char *argv[]) {
+
+}
