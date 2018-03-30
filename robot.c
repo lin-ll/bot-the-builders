721fa12f77a7fbb2690e663a1cf3e9b1da651ab3
danstan <ds32@Princeton.edu>
2018-03-30 16:08:18 -0400

    Updates to robot.c which will probably give conflicts

--- robot.c
+++ robot.c
@@ -1,1 +1,35 @@
 // The central file for controlling the robot
+
+float destX;
+float destY;
+
+int controls_finished = 0;
+
+
+
+
+while(True) {
+	// check buttons
+
+
+	// do controls thing
+	controls_finished = controls.update();
+	if(controls_finished){
+		int *up_wall, *down_wall, *left_wall, *right_wall;
+		contols.find_walls(up_wall, down_wall, left_wall, right_wall);
+		dir = maze.dfs(up_wall, down_wall, left_wall, right_wall);
+		controls.move(dir);
+	}
+
+
+	
+
+
+
+	//// do Kalman thing
+	// get all sensor values
+	// call Kalman with sensor values
+}
+
+
+
