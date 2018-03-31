52dae55d88f5e3d8c48bec0b9cbe41fc6cb978d0
danstan <ds32@Princeton.edu>
2018-03-31 00:07:29 -0400

    Updated motor test

--- test_motors.c
+++ test_motors.c
@@ -2,24 +2,104 @@
 #include <unistd.h>
 #include <stdio.h>
 
-
-#define UPPER_LEFT_FORWARD 21
-#define UPPER_RIGHT_FORWARD 23
-#define UPPER_LEFT_BACKWARD 22
-#define UPPER_RIGHT_BACKWARD 24
-#define LOWER_LEFT_FORWARD 27
-#define LOWER_RIGHT_FORWARD 31
-#define LOWER_LEFT_BACKWARD 28
-#define LOWER_RIGHT_BACKWARD 32
+#define UPPER_LEFT_FORWARD 16
+#define UPPER_LEFT_BACKWARD 15
+#define UPPER_RIGHT_FORWARD 22
+#define UPPER_RIGHT_BACKWARD 21
+#define LOWER_LEFT_FORWARD 32
+#define LOWER_LEFT_BACKWARD 31
+#define LOWER_RIGHT_FORWARD 23
+#define LOWER_RIGHT_BACKWARD 24
 
 int forward[4] = {UPPER_LEFT_FORWARD, UPPER_RIGHT_FORWARD, LOWER_LEFT_FORWARD, LOWER_RIGHT_FORWARD};
 int backward[4] = {UPPER_LEFT_BACKWARD, UPPER_RIGHT_BACKWARD, LOWER_LEFT_BACKWARD, LOWER_RIGHT_BACKWARD};
 
 int pi = 0;
 
+
+void set_forward(int speed) {
+	set_PWM_dutycycle(pi, forward[0], speed);
+	set_PWM_dutycycle(pi, backward[0], 0);
+	set_PWM_dutycycle(pi, forward[1], speed);
+	set_PWM_dutycycle(pi, backward[1], 0);
+	set_PWM_dutycycle(pi, forward[2], speed);
+	set_PWM_dutycycle(pi, backward[2], 0);
+	set_PWM_dutycycle(pi, forward[3], speed);
+	set_PWM_dutycycle(pi, backward[3], 0);
+}
+
+void set_backward(int speed) {
+	set_PWM_dutycycle(pi, forward[0], 0);
+	set_PWM_dutycycle(pi, backward[0], speed);
+	set_PWM_dutycycle(pi, forward[1], 0);
+	set_PWM_dutycycle(pi, backward[1], speed);
+	set_PWM_dutycycle(pi, forward[2], 0);
+	set_PWM_dutycycle(pi, backward[2], speed);
+	set_PWM_dutycycle(pi, forward[3], 0);
+	set_PWM_dutycycle(pi, backward[3], speed);
+}
+
+void set_right(int speed) {
+	set_PWM_dutycycle(pi, forward[0], speed);
+	set_PWM_dutycycle(pi, backward[0], 0);
+	set_PWM_dutycycle(pi, forward[1], 0);
+	set_PWM_dutycycle(pi, backward[1], speed);
+	set_PWM_dutycycle(pi, forward[2], 0);
+	set_PWM_dutycycle(pi, backward[2], speed);
+	set_PWM_dutycycle(pi, forward[3], speed);
+	set_PWM_dutycycle(pi, backward[3], 0);
+}
+
+void set_left(int speed) {
+	set_PWM_dutycycle(pi, forward[0], 0);
+	set_PWM_dutycycle(pi, backward[0], speed);
+	set_PWM_dutycycle(pi, forward[1], speed);
+	set_PWM_dutycycle(pi, backward[1], 0);
+	set_PWM_dutycycle(pi, forward[2], speed);
+	set_PWM_dutycycle(pi, backward[2], 0);
+	set_PWM_dutycycle(pi, forward[3], 0);
+	set_PWM_dutycycle(pi, backward[3], speed);
+}
+
+void set_off() {
+	set_PWM_dutycycle(pi, forward[0], 0);
+	set_PWM_dutycycle(pi, backward[0], 0);
+	set_PWM_dutycycle(pi, forward[1], 0);
+	set_PWM_dutycycle(pi, backward[1], 0);
+	set_PWM_dutycycle(pi, forward[2], 0);
+	set_PWM_dutycycle(pi, backward[2], 0);
+	set_PWM_dutycycle(pi, forward[3], 0);
+	set_PWM_dutycycle(pi, backward[3], 0);
+}
+
+
 int main(){
 	pi = pigpio_start(NULL, NULL);
-	
+
+	set_forward(127);
+	usleep(1000000);
+	set_off();
+	usleep(1000000);
+
+	set_right(127);
+	usleep(1000000);
+	set_off();
+	usleep(1000000);
+
+	set_backward(127);
+	usleep(1000000);
+	set_off();
+	usleep(1000000);
+
+	set_left(127);
+	usleep(1000000);
+	set_off();
+	usleep(1000000);
+
+
+
+
+	/*
 	
 	for(int i=0; i<1000; i++){
 		for(int j=0; j<3; j++){
@@ -51,10 +131,7 @@ int main(){
 			set_PWM_dutycycle(pi, UPPER_LEFT_BACKWARD, 0);
 			usleep(1000000);
 		}
-
-		double reading = Sensor_getShort(3);
-		printf("reading: %f\n", reading);
-		usleep(100000);
 	}
+	*/
 	return 0;
 }
