bd82fc2ae3c0749e0f36c4f45ded53cd90fec8fc
danstan <ds32@Princeton.edu>
2018-03-30 22:46:41 -0400

    Updated kalman.h and did some stuff to robot.c apparently

--- robot.c
+++ robot.c
@@ -8,20 +8,20 @@
 #include "inc/heap.h"
 
 int explore() {
-    int explorationVelocity = 10; // speed for exploration
-    int row = 0;
-    int col = 0;
-    int currNode = getIntFromCoordinates(row, col);
-    while (!isExplored()) {
-        // gather information about walls from sensors
-        int leftWall = 0;
-        int rightWall = 0;
-        int upWall = 0;
-        int downWall = 0;
-        int nextNode = dfs(row, col, leftWall, rightWall, upWall, downWall);
-        moveTo(currNode, nextNode);
-        currNode = nextNode;
-    }
+	int explorationVelocity = 10; // speed for exploration
+	int row = 0;
+	int col = 0;
+	int currNode = getIntFromCoordinates(row, col);
+	while (!isExplored()) {
+		// gather information about walls from sensors
+		int leftWall = 0;
+		int rightWall = 0;
+		int upWall = 0;
+		int downWall = 0;
+		int nextNode = dfs(row, col, leftWall, rightWall, upWall, downWall);
+		moveTo(currNode, nextNode);
+		currNode = nextNode;
+	}
 }
 
 void traverseShortestPath(heap_t *path, int start) {
@@ -46,41 +46,47 @@ void solveMaze(Graph g) {
 }
 
 void main() {
-    explore();
-    returnToStart();
-    solveMaze();
+	explore();
+	returnToStart();
+	solveMaze();
 
 
 
-    // from danstan
-    float destX;
-    float destY;
-
-    int controls_finished = 0;
+	// from danstan
+	float destX;
+	float destY;
 
 
+	int controls_finished = 1;
 
 
-	while(True) {
-	// check buttons
 
 
-	// do controls thing
-	controls_finished = controls.update();
-	if(controls_finished){
-		int *up_wall, *down_wall, *left_wall, *right_wall;
-		contols.find_walls(up_wall, down_wall, left_wall, right_wall);
-		dir = maze.dfs(up_wall, down_wall, left_wall, right_wall);
-		controls.move(dir);
+	while(1) {
+		// check buttons
+
+
+		// do controls thing
+		if(controls_finished){
+			int *up_wall, *down_wall, *left_wall, *right_wall;
+			contols.find_walls(up_wall, down_wall, left_wall, right_wall);
+			dir = maze.dfs(up_wall, down_wall, left_wall, right_wall);
+
+			if(dir == -1){
+				// we did it!
+				controls.stop();
+				break;
+			}
+
+			controls.set_new_dest(dir);
+		}
+		
+
+		controls_finished = controls.update();
+
+		//// do Kalman thing
+		// get all sensor values
+		// call Kalman with sensor values
 	}
-
-
-	
-
-
-
-	//// do Kalman thing
-	// get all sensor values
-	// call Kalman with sensor values
 }
 
