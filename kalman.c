bda3fae17bdc20af3ff68ef06d5ab86583a54a1a
danstan <ds32@Princeton.edu>
2018-04-05 22:46:40 -0400

    Added test_kalan, totally untested

--- kalman.c
+++ kalman.c
@@ -40,27 +40,27 @@ double R[NUM*NUM] =
 			 0.0, 0.0, 0.0, 0.0, 0.01, 0.0,
 			 0.0, 0.0, 0.0, 0.0, 0.0, 0.01};
 
-double kalman_getX() {
+double Kalman_getX() {
 		return x[0];
 }
 
-double kalman_getY() {
+double Kalman_getY() {
 		return x[1];
 }
 
-double kalman_getVx() {
+double Kalman_getVx() {
 		return x[3];
 }
 
-double kalman_getVy() {
+double Kalman_getVy() {
 		return x[4];
 }
 
-double kalman_getT() {
+double Kalman_getT() {
 		return x[2];
 }
 
-double kalman_getVt() {
+double Kalman_getVt() {
 		return x[5];
 }
 
@@ -120,8 +120,8 @@ void mat_vec_mult(double *A, double *x, double *dest){
 }
 
 void init(){
-	x[0] = 1.0; // x
-	x[1] = 1.0; // y
+	x[0] = 90.0; // x
+	x[1] = 90.0; // y
 	x[2] = 0.0; // t
 	x[3] = 0.0; // vx
 	x[4] = 0.0; // vy
@@ -153,14 +153,15 @@ double interpret_distance_aux(double pos, double dist, double *estimate, int *co
 }
 
 /* given what the distance sensors see, interpret where we think we are */
+/* Distance sensors are +y,-y,+x,-x for short, then those four for long */
 void interpret_distance(double *distances, double *x_pos, double *y_pos){
 	// TODO: take into account the spin of the vehicle?
 	double x_estimate = 0;
 	int x_count = 0;
-	interpret_distance_aux(x[0],  distances[0], &x_estimate, &x_count); //  x, short
-	interpret_distance_aux(x[0], -distances[1], &x_estimate, &x_count); // -x, short
-	interpret_distance_aux(x[0],  distances[4], &x_estimate, &x_count); //  x, long
-	interpret_distance_aux(x[0], -distances[5], &x_estimate, &x_count); // -x, long
+	interpret_distance_aux(x[0],  distances[3], &x_estimate, &x_count); //  x, short
+	interpret_distance_aux(x[0], -distances[2], &x_estimate, &x_count); // -x, short
+	interpret_distance_aux(x[0],  distances[7], &x_estimate, &x_count); //  x, long
+	interpret_distance_aux(x[0], -distances[6], &x_estimate, &x_count); // -x, long
 	if(x_count == 0){
 		// we got no readings for x!
 		x_estimate = NAN;
@@ -170,10 +171,10 @@ void interpret_distance(double *distances, double *x_pos, double *y_pos){
 
 	double y_estimate = 0;
 	int y_count = 0;
-	interpret_distance_aux(x[1],  distances[2], &y_estimate, &y_count); //  y, short
-	interpret_distance_aux(x[1], -distances[3], &y_estimate, &y_count); // -y, short
-	interpret_distance_aux(x[1],  distances[6], &y_estimate, &y_count); //  y, long
-	interpret_distance_aux(x[1], -distances[7], &y_estimate, &y_count); // -y, long
+	interpret_distance_aux(x[1],  distances[0], &y_estimate, &y_count); //  y, short
+	interpret_distance_aux(x[1], -distances[1], &y_estimate, &y_count); // -y, short
+	interpret_distance_aux(x[1],  distances[4], &y_estimate, &y_count); //  y, long
+	interpret_distance_aux(x[1], -distances[5], &y_estimate, &y_count); // -y, long
 	if(y_count == 0){
 		// we got no readings for y!
 		y_estimate = NAN;
@@ -192,6 +193,38 @@ double interpret_encoders(double *encoders, double *vx, double *vy, double *vt){
 	*vt = encoders[0]+encoders[1]+encoders[2]+encoders[3];
 	return encoders[0]-encoders[1]+encoders[2]-encoders[3];
 }
+
+double combine_vt(double encoder, double gyro){
+	// TODO do better than this if we fix encoders
+	return gyro;
+}
+
+
+
+
+void update(double dt, double *control){
+	
+	// TODO :(
+	double encoders[4];
+	for(int i=0; i<3; i++){
+		encoders[4] = NAN;
+	}
+
+	double distances[8];
+	for(int i=0; i<3; i++){
+		distances[i] = Sensor_getShort(i);
+	}
+	for(int i=0; i<3; i++){
+		distances[4+i] = Sensor_getLong(i);
+	}
+
+	double gyro = Sensor_getGyro();
+	double compass = NAN;
+
+	update_given_sensors(dt, encoders, distances, gyro, compass, control);
+
+}
+
 /**
  * update with new sensor values
  * send 8 distance sensors, 4 encoders, accx / accy / gyro,
@@ -199,11 +232,13 @@ double interpret_encoders(double *encoders, double *vx, double *vy, double *vt){
  *
  * NAN for any sensor values means we don't have a reading for whatever reason (no wall nearby)
  *
- * Distance sensors are +x,-x,+y,-y for short, then those four for long
+ * Distance sensors are +y,-y,+x,-x for short, then those four for long
  *
  **/
-void update(double *distances, double *encoders, double *imu, double *control, double *pos){
+void Kalman_update_given_sensors(double dt, double *encoders, double *distances, double gyro, double compass, double control){
+
 	double dt = 0.05; // TODO use an actual timer
+
 
 	/* First, the predict step. Without sensors, what do we guess the new x is?
 	   We write a matrix that will take old x to new x */
@@ -235,13 +270,25 @@ void update(double *distances, double *encoders, double *imu, double *control, d
 
 
 	/* Measurement step, */
+	/* we're working hard to do all this interpretation beforehand so the matrix that transforms our
+	   sensor readings into the right dimensions (H in the tutorial) is just the identity */
+
 	double dist_x,dist_y; // measured x and y location
 	interpret_distance(distances, &dist_x, &dist_y);
 
-	double encoder_vx, encoder_vy, encoder_vt;
-	interpret_encoders(encoders, &encoder_vx, &encoder_vy, &encoder_vt);
-	/* we're working hard to do all this interpretation beforehand so the matrix that transforms our
-	   sensor readings into the right dimensions (H in the tutorial) is just the identity */
+
+	// TODO redo this if we fix encoders
+	//double encoder_vx, encoder_vy, encoder_vt;
+	//nterpret_encoders(encoders, &encoder_vx, &encoder_vy, &encoder_vt);
+	double encoder_vx = NAN;
+	double encoder_vy = NAN;
+	double encoder_vt = NAN;
+
+
+	double vt_estimate = combine_vt(encoder_vt, gyro);
+
+
+
 	if (isnan(dist_x)) {
 			z[0] = x_hat[0];
 			R[0][0] = 40000;
