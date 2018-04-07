49b08d57c78ea224314b4a091c581c0b4f20d9c7
danstan <ds32@Princeton.edu>
2018-04-07 10:22:59 -0400

    Looped leds

--- test_leds.c
+++ test_leds.c
@@ -13,44 +13,47 @@ int main() {
 	int sleep = 500000;
 
 	// red
-	Led_setColor(MAX_COLOR, 0, 0);
-	usleep(time);
-	Led_off();
-	usleep(sleep);
 
-	// green
-	Led_setColor(0, MAX_COLOR, 0);
-	usleep(time);
-	Led_off();
-	usleep(sleep);
+	while(1){
+		Led_setColor(MAX_COLOR, 0, 0);
+		usleep(time);
+		Led_off();
+		usleep(sleep);
 
-	// blue
-	Led_setColor(0, 0, MAX_COLOR);
-	usleep(time);
-	Led_off();
-	usleep(sleep);
+		// green
+		Led_setColor(0, MAX_COLOR, 0);
+		usleep(time);
+		Led_off();
+		usleep(sleep);
 
-	// red + green = yellow
-	Led_setColor(MAX_COLOR, MAX_COLOR, 0);
-	usleep(time);
-	Led_off();
-	usleep(sleep);
+		// blue
+		Led_setColor(0, 0, MAX_COLOR);
+		usleep(time);
+		Led_off();
+		usleep(sleep);
 
-	// red + blue = magenta
-	Led_setColor(MAX_COLOR, 0, MAX_COLOR);
-	usleep(time);
-	Led_off();
-	usleep(sleep);
+		// red + green = yellow
+		Led_setColor(MAX_COLOR, MAX_COLOR, 0);
+		usleep(time);
+		Led_off();
+		usleep(sleep);
 
-	// green + blue = cyan
-	Led_setColor(0, MAX_COLOR, MAX_COLOR);
-	usleep(time);
-	Led_off();
-	usleep(sleep);
+		// red + blue = magenta
+		Led_setColor(MAX_COLOR, 0, MAX_COLOR);
+		usleep(time);
+		Led_off();
+		usleep(sleep);
 
-	// red + green + blue = white
-	Led_setColor(MAX_COLOR, MAX_COLOR, MAX_COLOR);
-	usleep(time);
-	Led_off();
-	usleep(sleep);
+		// green + blue = cyan
+		Led_setColor(0, MAX_COLOR, MAX_COLOR);
+		usleep(time);
+		Led_off();
+		usleep(sleep);
+
+		// red + green + blue = white
+		Led_setColor(MAX_COLOR, MAX_COLOR, MAX_COLOR);
+		usleep(time);
+		Led_off();
+		usleep(sleep);
+	}
 }
