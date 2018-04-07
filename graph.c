e699ba48805382938b1c423e6a320aff319670eb
czye17 <czye@princeton.edu>
2018-04-07 04:17:13 -0400

    updated graph

--- graph.c
+++ graph.c
@@ -1,167 +1,103 @@
-/*----------------------------------------------------------------------------*/
-/* Taken from Yale CS.                                                        */
-//* http://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)Graphs.html              */                                               */
-/*----------------------------------------------------------------------------*/
-
+/*graph.c*/
+#include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-
 #include "graph.h"
 
-/* basic directed graph type */
-/* the implementation uses adjacency lists
- * represented as variable-length arrays */
-
-/* these arrays may or may not be sorted: if one gets long enough
- * and you call graph_has_edge on its source, it will be */
-
-struct successors {
-    int d;          /* number of successors */
-    int len;        /* number of slots in array */
-    char is_sorted; /* true if list is already sorted */
-    int list[1];    /* actual list of successors */
-};
-
-struct graph {
-    int n;              /* number of vertices */
-    int m;              /* number of edges */
-    struct successors *alist[1];
-};
-
-/* create a new graph with n vertices labeled 0..n-1 and no edges */
-Graph graph_create(int n)
+/* Function to create an adjacency list node*/
+adjlist_node_p createNode(int v)
 {
-    Graph g;
+    adjlist_node_p newNode = (adjlist_node_p)malloc(sizeof(adjlist_node_t));
+    if(!newNode)
+        err_exit("Unable to allocate memory for new node");
+
+    newNode->vertex = v;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+/* Function to create a graph with n vertices; Creates both directed and undirected graphs*/
+graph_p createGraph(int n, graph_type_e type)
+{
     int i;
+    graph_p graph = (graph_p)malloc(sizeof(graph_t));
+    if(!graph)
+        err_exit("Unable to allocate memory for graph");
+    graph->num_vertices = n;
+    graph->type = type;
 
-    g = (Graph) malloc(sizeof(struct graph) + sizeof(struct successors *) * (n-1));
-    assert(g);
+    /* Create an array of adjacency lists*/
+    graph->adjListArr = (adjlist_p)malloc(n * sizeof(adjlist_t));
+    if(!graph->adjListArr)
+        err_exit("Unable to allocate memory for adjacency list array");
 
-    g->n = n;
-    g->m = 0;
-
-    for(i = 0; i < n; i++) {
-        g->alist[i] = (successors *)malloc(sizeof(struct successors));
-        assert(g->alist[i]);
-
-        g->alist[i]->d = 0;
-        g->alist[i]->len = 1;
-        g->alist[i]->is_sorted= 1;
+    for(i = 0; i < n; i++)
+    {
+        graph->adjListArr[i].head = NULL;
+        graph->adjListArr[i].num_members = 0;
     }
 
-    return g;
+    return graph;
 }
 
-/* free all space used by graph */
-void graph_destroy(Graph g)
+/*Destroys the graph*/
+void destroyGraph(graph_p graph)
 {
-    int i;
-
-    for(i = 0; i < g->n; i++) free(g->alist[i]);
-    free(g);
-}
-
-/* add an edge to an existing graph */
-void graph_add_edge(Graph g, int u, int v)
-{
-    assert(u >= 0);
-    assert(u < g->n);
-    assert(v >= 0);
-    assert(v < g->n);
-
-    /* do we need to grow the list? */
-    while(g->alist[u]->d >= g->alist[u]->len) {
-        g->alist[u]->len *= 2;
-        g->alist[u] = (successors *)
-            realloc(g->alist[u],
-                sizeof(struct successors) + sizeof(int) * (g->alist[u]->len - 1));
-    }
-
-    /* now add the new sink */
-    g->alist[u]->list[g->alist[u]->d++] = v;
-    g->alist[u]->is_sorted = 0;
-
-    /* bump edge count */
-    g->m++;
-}
-
-/* return the number of vertices in the graph */
-int graph_vertex_count(Graph g)
-{
-    return g->n;
-}
-
-/* return the number of edges in the graph */
-int graph_edge_count(Graph g)
-{
-    return g->m;
-}
-
-/* return the out-degree of a vertex */
-int graph_out_degree(Graph g, int source)
-{
-    assert(source >= 0);
-    assert(source < g->n);
-
-    return g->alist[source]->d;
-}
-
-
-static int intcmp(const void *a, const void *b)
-{
-    return *((const int *) a) - *((const int *) b);
-}
-
-/* return 1 if edge (source, sink) exists), 0 otherwise */
-int graph_has_edge(Graph g, int source, int sink)
-{
-    int i;
-
-    assert(source >= 0);
-    assert(source < g->n);
-    assert(sink >= 0);
-    assert(sink < g->n);
-
-    if(graph_out_degree(g, source) >= BSEARCH_THRESHOLD) {
-        /* make sure it is sorted */
-        if(! g->alist[source]->is_sorted) {
-            qsort(g->alist[source]->list,
-                    g->alist[source]->d,
-                    sizeof(int),
-                    intcmp);
+    if(graph)
+    {
+        if(graph->adjListArr)
+        {
+            int v;
+            /*Free up the nodes*/
+            for (v = 0; v < graph->num_vertices; v++)
+            {
+                adjlist_node_p adjListPtr = graph->adjListArr[v].head;
+                while (adjListPtr)
+                {
+                    adjlist_node_p tmp = adjListPtr;
+                    adjListPtr = adjListPtr->next;
+                    free(tmp);
+                }
+            }
+            /*Free the adjacency list array*/
+            free(graph->adjListArr);
         }
-
-        /* call bsearch to do binary search for us */
-        return
-            bsearch(&sink,
-                    g->alist[source]->list,
-                    g->alist[source]->d,
-                    sizeof(int),
-                    intcmp)
-            != 0;
-    } else {
-        /* just do a simple linear search */
-        /* we could call lfind for this, but why bother? */
-        for(i = 0; i < g->alist[source]->d; i++) {
-            if(g->alist[source]->list[i] == sink) return 1;
-        }
-        /* else */
-        return 0;
+        /*Free the graph*/
+        free(graph);
     }
 }
 
-/* invoke f on all edges (u,v) with source u */
-/* supplying data as final parameter to f */
-void graph_foreach(Graph g, int source,
-    void (*f)(Graph g, int source, int sink, void *data),
-    void *data)
+/* Adds an edge to a graph*/
+void addEdge(graph_t *graph, int src, int dest)
 {
-    int i;
+    /* Add an edge from src to dst in the adjacency list*/
+    adjlist_node_p newNode = createNode(dest);
+    newNode->next = graph->adjListArr[src].head;
+    graph->adjListArr[src].head = newNode;
+    graph->adjListArr[src].num_members++;
 
-    assert(source >= 0);
-    assert(source < g->n);
-
-    for(i = 0; i < g->alist[source]->d; i++) {
-        f(g, source, g->alist[source]->list[i], data);
+    if(graph->type == UNDIRECTED)
+    {
+        /* Add an edge from dest to src also*/
+        newNode = createNode(src);
+        newNode->next = graph->adjListArr[dest].head;
+        graph->adjListArr[dest].head = newNode;
+        graph->adjListArr[dest].num_members++;
     }
+}
+
+/* Returns 1 if edge exists, 0 otherwise */
+int hasEdge(graph_t *graph, int src, int dest)
+{
+    adjlist_node_p curr = graph->adjListArr[src].head;
+    while (curr != NULL)
+    {
+        int check = curr->vertex;
+        if (dest == check)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
 }
